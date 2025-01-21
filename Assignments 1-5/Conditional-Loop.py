635c7c98db97f3a2460572f53cf2c58bfc01fee5
Tsunashiko <Schell.Kainoa@student.greenriver.edu>
2025-01-21 11:18:56 -0800

    Add function send_message()

--- Assignments 1-5/Conditional-Loop.py
+++ Assignments 1-5/Conditional-Loop.py
@@ -1,11 +1,22 @@
-def askHowDay():
-    question = input("Is today going to be a great day? Yes/No").capitalize()
-    if question == "Yes":
-        for question in range(10):
-            print("Of course it is!")
-    elif question == "No":
-        print("Think more positively")
-    else:
-        print("Sorry please reinput I didn't understand")
+"""
+Script asks user if today is a good day and includes function to print iteration x-10
+"""
 
-askHowDay()
+def send__message():
+    """
+    Prints positive message 10 times
+    """
+    for question in range(10):
+            print("Of course it is!")
+
+question = input("Is today going to be a great day? Yes/No").capitalize()
+
+if question == "Yes":
+        send__message()
+
+elif question == "No":
+    print("Think more positively")
+
+else:
+    print("Sorry please reinput I didn't understand")
+
